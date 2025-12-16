b06a61c953a01211f593fd81f55fe7606102038d
EmmaThdDev <emman@hayooapp.com>
2025-12-16 09:47:05 +0200

    fixed cors

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,12 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+export const generateToken = (id) => {
+  // If JWT_EXPIRES_IN is set to the string 'never', create a token without expiration.
+  // Otherwise, pass the expiresIn option if provided.
+  const options = {};
+  if (process.env.JWT_EXPIRES_IN && process.env.JWT_EXPIRES_IN !== 'never') {
+    options.expiresIn = process.env.JWT_EXPIRES_IN;
+  }
+
+  return jwt.sign({ id }, process.env.JWT_SECRET, Object.keys(options).length ? options : undefined);
+};
